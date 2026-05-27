95d8f3764bcd2585e3f278b399f916f7afc678cd
Jesús Iván Hernández Sarabia <80736529+Ivans-Hernandez@users.noreply.github.com>
2026-05-26 18:11:20 -0600

    Add function to count special characters
    Implement function to count special characters in a word.

--- Easy/3120-special-characters.c
+++ Easy/3120-special-characters.c
@@ -1,1 +1,28 @@
+int numberOfSpecialChars(char* word) {
 
+    int minuscula[26] = {0};
+    int mayuscula[26] = {0};
+    int count = 0;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+
+        if (islower(word[i])) { // palabra reservada que comprueba si es minuscula
+            minuscula[word[i] - 'a'] = 1;
+        }
+
+        if (isupper(word[i])) { // palabra reservada que comprueba si es mayuscula
+            mayuscula[word[i] - 'A'] = 1;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (minuscula[i] && mayuscula[i]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+    
